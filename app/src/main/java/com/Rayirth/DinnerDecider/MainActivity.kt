25c7cc19275f4a64431785a6c39e3f7c51591c9f
Rayirth Jaiswal <rayirthjaiswal27@gmail.com>
2023-04-27 20:15:48 +0530

    Changes made in the UI

--- app/src/main/java/com/Rayirth/DinnerDecider/MainActivity.kt
+++ app/src/main/java/com/Rayirth/DinnerDecider/MainActivity.kt
@@ -3,8 +3,6 @@
 package com.Rayirth.DinnerDecider
 
 import android.os.Bundle
-import android.view.Window
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.Rayirth.DinnerDecider.databinding.ActivityMainBinding
 import java.util.*
@@ -18,11 +16,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-        supportActionBar?.hide()
-
 
 
         binding = ActivityMainBinding.inflate(layoutInflater)
