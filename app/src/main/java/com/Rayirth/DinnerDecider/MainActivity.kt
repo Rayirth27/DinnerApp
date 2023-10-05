b710885021194ef58f13379b8a067c001294dca6
Rayirth <rayirthjaiswal27@gmail.com>
2023-10-05 23:36:00 +0530

    The floating text has been added which shows that the item entered by the user has been added into the list :)

--- app/src/main/java/com/Rayirth/DinnerDecider/MainActivity.kt
+++ app/src/main/java/com/Rayirth/DinnerDecider/MainActivity.kt
@@ -5,6 +5,7 @@ package com.Rayirth.DinnerDecider
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.Rayirth.DinnerDecider.databinding.ActivityMainBinding
 import java.util.*
@@ -14,13 +15,26 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    val foodList = arrayListOf("Chinese","Hamburger","Pizza","Mc Donald's","Idli Dosa","Nan Kulcha","Dahi Paratha","Palak Paneer")
+    val foodList = arrayListOf(
+        "Soya Chaap",
+        "Hamburger",
+        "Chilli Chicken",
+        "Pizza",
+        "Mc Donald's",
+        "Idli Dosa",
+        "Nan Kulcha",
+        "Dahi Paratha",
+        "Palak Paneer"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE)
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        this.getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         //supportActionBar?.hide()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,11 +48,15 @@ class MainActivity : AppCompatActivity() {
             binding.selectedFoodTxt.text = foodList[randomFood]
         }
 
-        binding.addFoodBtn.setOnClickListener{
+        binding.addFoodBtn.setOnClickListener {
             val newFood = binding.addFoodTxt.text.toString()
-            foodList.add(newFood)
-            binding.addFoodTxt.text.clear()
 
+            if (newFood != "") {
+                foodList.add(newFood)
+                binding.addFoodTxt.text.clear()
+                Toast.makeText(this,"Your Item has been added :)", Toast.LENGTH_LONG).show()
+
+            }
         }
     }
 }
